dd0a1ddc545394a2be6e0994c0ff7798e8282be4
pampang <fspwz@sina.com>
2016-03-16 23:47:23 +0800

    第十章

--- app/main.js
+++ app/main.js
@@ -2,6 +2,6 @@ import React from 'react';
 import Router from 'react-router';
 import routes from './routes';
 
-Router.run(routes, Routre.HistoryLocation, function (Handler) {
-    React.render(<Handler />, doucment.getElementById('app'));
+Router.run(routes, Router.HistoryLocation, function (Handler) {
+    React.render(<Handler />, document.getElementById('app'));
 });
